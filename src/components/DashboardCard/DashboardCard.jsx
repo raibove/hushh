15c5c6d55fa4b9ba86707b28700900f85129f1de
raibove <shwetakale144@gmail.com>
2023-02-13 11:37:55 +0530

    update to js

--- src/components/DashboardCard/DashboardCard.jsx
+++ src/components/DashboardCard/DashboardCard.jsx
@@ -1,6 +1,14 @@
 import "./DashboardCard.css"
+import Modal from '@mui/material/Modal';
+import { useState } from "react";
 
-function DashboardCard({imageSrc, altText}:any) {
+function DashboardCard({imageSrc, altText}) {
+    
+  const [open, setOpen] = useState(false);
+  
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
     return (
         <div className="dashboard-card">
             <div className="dashboard-card-img-container">
@@ -9,6 +17,10 @@ function DashboardCard({imageSrc, altText}:any) {
             <div className="dashboard-card-title">
                 <p className="dashboard-card-p">An interview for job?</p>
             </div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+            ></Modal>
         </div>
     )
 }
