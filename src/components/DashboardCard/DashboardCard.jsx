800a9beec375567caf11747368616756e9b5ca35
raibove <shwetakale144@gmail.com>
2023-02-14 11:45:52 +0530

    add modal

--- src/components/DashboardCard/DashboardCard.jsx
+++ src/components/DashboardCard/DashboardCard.jsx
@@ -3,25 +3,30 @@ import Modal from '@mui/material/Modal';
 import { useState } from "react";
 
 function DashboardCard({imageSrc, altText}) {
-    
-  const [open, setOpen] = useState(false);
-  
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     return (
-        <div className="dashboard-card">
+        <>
+        <div className="dashboard-card" onClick={handleOpen}>
             <div className="dashboard-card-img-container">
                 <img src={imageSrc} alt={altText} className="dashboard-card-img"/>
             </div>
             <div className="dashboard-card-title">
                 <p className="dashboard-card-p">An interview for job?</p>
             </div>
-            <Modal
-                open={open}
-                onClose={handleClose}
-            ></Modal>
         </div>
+        <Modal
+            open={open}
+            onClose={handleClose}
+        >
+            <div  className="modal">
+                <img src={imageSrc} alt={altText} className="modal-img"/>
+                <span className="modal-description">apple</span>
+            </div>
+        </Modal>
+        </>
     )
 }
 
--- src/pages/DashboardPage/Dashboard.jsx
+++ src/pages/DashboardPage/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 import logo from "../../assets/logo.svg"
 import debate from "../../assets/debate.svg"
 import sales_call from "../../assets/sales_call.svg"
@@ -9,16 +10,17 @@ import DashboardCard from "../../components/DashboardCard/DashboardCard"
 import "./Dashboard.css"
 
 export default function DashboardPage() {
+    
     const navigate = useNavigate()
     return(
         <>
-            <div className="logo-container" onClick={()=>{navigate('/')}}>
-                <img src={logo} alt="hushh" className="logo"/>
+            <div className="logo-container" >
+                <img src={logo} alt="hushh" onClick={()=>{navigate('/')}} className="logo"/>
             </div>
             <div className="dashboard-card-container">
-                <DashboardCard imageSrc={debate} altText="debate"/>
+                <DashboardCard imageSrc={debate} altText="debate" />
                 <DashboardCard imageSrc={sales_call} altText="sales call" />
-                <DashboardCard imageSrc={interview_colorful} altText="interview call" />
+                <DashboardCard imageSrc={interview_colorful} altText="interview call"/>
                 <DashboardCard imageSrc={date} altText="date"/>
                 <DashboardCard imageSrc={debate_color} altText="debate"/>
             </div>
